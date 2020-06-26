2ba6af16645cd767f431d52e2d18bea96c78d50b
Vinod Kumavat <u19ee027@eed.svnit.ac.in>
2020-06-26 15:10:00 +0530

    Create zip_converter.py

---
+++ 3. Zip Converter/zip_converter.py
@@ -0,0 +1,14 @@
+import patoolib
+
+
+def zipConvert():
+    # (namebof file.extension to convert, (actual file that need to be converted, 2nd file to be converted) )
+    # 1st and 2nd file will be converted into zip
+    patoolib.create_archive("test.zip", ("instruction.txt", "ttp.py"))
+    print("File zipped successfully....")
+
+
+def unZip():
+    # ("name of zip file to extract", outdir="location to store extracted file")
+    patoolib.extract_archive("test.zip", outdir="extract")
+    print("File un-zipped successfully....")
