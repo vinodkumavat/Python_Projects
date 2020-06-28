ec6f9175aa5c1aa9d5747155174548c8f924cd37
Vinod Kumavat <u19ee027@eed.svnit.ac.in>
2020-06-28 21:27:36 +0530

    Update zip_converter.py

--- 3. Zip Converter/zip_converter.py
+++ 3. Zip Converter/zip_converter.py
@@ -4,7 +4,7 @@ import patoolib
 def zipConvert():
     # (namebof file.extension to convert, (actual file that need to be converted, 2nd file to be converted) )
     # 1st and 2nd file will be converted into zip
-    patoolib.create_archive("test.zip", ("instruction.txt", "ttp.py"))
+    patoolib.create_archive("test.zip", ("instruction.txt", "ttp.txt"))
     print("File zipped successfully....")
 
 
